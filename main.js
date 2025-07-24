2a94d81b9113063794e4f5956d7ae5b171d9b892
feras-salloum <ferassalloum1999@gmail.com>
2025-07-24 13:05:56 +0300

    all

--- main.js
+++
@@ -1,6 +0,0 @@
-function calculateSacrificeProfit(buyPrices, sellPrices) {
-    let allBuy = buyPrices.reduce((acc, css) => acc + css, 0);
-    let allSell = sellPrices.reduce((acc, css) => acc + css, 0);
-    let result = allSell - allBuy;
-    return result
-}
--- main2.js
+++
@@ -1,4 +0,0 @@
-function calculateSacrificeProfit(buyPrices, sellPrices) {
-    const profits = buyPrices.map((buyPrice, i) => sellPrices[i] - buyPrice);
-    return profits.reduce((sum, profit) => sum + profit, 0);
-} 
