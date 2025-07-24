eb2ab45e1d6c206768b1c762445bd3e58ebd7f31
feras-salloum <ferassalloum1999@gmail.com>
2025-07-24 11:32:24 +0300

    edit main2.js

--- main2.js
+++ main2.js
@@ -1,1 +1,4 @@
-"function calculateSacrificeProfit(buyPrices, sellPrices) {   const profits = buyPrices.map((buyPrice, i) => sellPrices[i] - buyPrice);   return profits.reduce((sum, profit) => sum + profit, 0); }" 
+function calculateSacrificeProfit(buyPrices, sellPrices) {
+    const profits = buyPrices.map((buyPrice, i) => sellPrices[i] - buyPrice);
+    return profits.reduce((sum, profit) => sum + profit, 0);
+} 
